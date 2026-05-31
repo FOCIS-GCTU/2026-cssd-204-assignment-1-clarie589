2fa42e2f6b10e4d8bd1f153282e951efe350d601
clarie589 <2425401949@live.gctu.edu.gh>
2026-05-31 13:38:00 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: Assignment 2
+#
+# Name: Anne-Marie Clarkson
+# STUDENT ID:  2425401949
+# Email: 2425401949@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Anne-marie Clarkson, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    print()
+    print("...AMC")
+    print()
+    print(".......AAAA............MM........MM............CCCC.........")
+    print("......AA..AA...........MMM......MMM..........CCCCCCCC.......")
+    print(".....AA....AA..........MMMM....MMMM.........CC......CC......")
+    print(".....AA....AA..........MM.MM..MM.MM........CC........CC.....")
+    print("....AAAAAAAAAA.........MM..MMMM..MM........CC...............")
+    print("....AAAAAAAAAA.........MM...MM...MM........CC...............")
+    print("...AA........AA........MM........MM........CC........CC.....")
+    print("...AA........AA........MM........MM.........CC......CC......")
+    print("...AA........AA...**...MM........MM...**.....CCCCCCCC.....**")
+    print("...AA........AA...**...MM........MM...**.......CCCC.......**")
+    print()
 
 
 main()
